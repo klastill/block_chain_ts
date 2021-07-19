945e63f49ed518282830f9ea892a59c2563098af
klastill <gyb0501@gmail.com>
2021-07-19 13:48:27 +0900

    create block

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("hi");
+class Block {
+  public index: number;
+  public hash: string;
+  public prevHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    prevHash: string,
+    data: string,
+    timestamp: number
+  ){
+    this.index = index;
+    this.hash = hash;
+    this.prevHash = prevHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
+
+const genesisBlock: Block = new Block(0, "000000000000", "", "initial", 20210719);
+
+let blockchain: [Block] = [genesisBlock];
+
+console.log(blockchain);
+
+export {};
